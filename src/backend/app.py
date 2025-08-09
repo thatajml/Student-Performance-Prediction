ffad0fd001dbba490b483a8b086a39c89153b431
thatajml <thatajml@icloud.com>
2025-08-09 20:25:51 +0530

    placeholder code

--- src/backend/app.py
+++ src/backend/app.py
@@ -1,10 +1,10 @@
 from flask import Flask
-from routes.prediction_routes import prediction_bp
 
 app = Flask(__name__)
 
-# Register routes
-app.register_blueprint(prediction_bp, url_prefix="/api")
+@app.route("/")
+def hello():
+    return "Hello World!"
 
 if __name__ == "__main__":
     app.run(debug=True)
